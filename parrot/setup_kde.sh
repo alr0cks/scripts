ced21582ee9c825d11d4d46716e0d0b1e006b01f
alr0cks <alok.bhawankar@gmail.com>
2020-08-13 01:49:21 +0530

    removed configs cuz no need

--- parrot/setup_kde.sh
+++ parrot/setup_kde.sh
@@ -3,6 +3,3 @@
 sudo apt install parrot-kde sddm 
 
 sudo systemctl enable sddm.service
-
-cp -r -v Dotfiles/.config/* ~/.config/
-cp -r -v Dotfiles/.local/* ~/.local/
--- parrot/setup_main.sh
+++ parrot/setup_main.sh
@@ -11,9 +11,6 @@ chsh -s /usr/bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-#update zshrc
-cp Dotfiles/.zshrc ~/
-
 #install spotify
 curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add - 
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
